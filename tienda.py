d89e3eccc83a469c4a24eff4049c8753c94a26a4
Pedro <mendez.pedro94@gmail.com>
2024-05-07 23:51:56 -0400

    new_solution

--- tienda.py
+++ tienda.py
@@ -32,52 +32,117 @@ class Restaurante(Tienda):
         self.__name = name
         self.__delivery_cost = delivery_cost
         self.__product_list = []
-
-    @property
-    def get_name(self):
-        return self.__name
     
-    @property
-    def get_delivery_cost(self):
-        return self.__delivery_cost
-    
-    def ingress_products(self, product_name:str, product_cost:int, product_stock:int = 0):
-        for product in self.__product_list:
-            print(product.get_name)
-            if product.get_name == product_name:
-                product.set_stock += product_stock
-                return
-        p = Product(product_name, product_cost, product_stock)
-        self.__product_list.append(p)
+    def ingress_products(self, name:str, cost:int, stock:int = 0):
+        p = Product(name, cost)
+        encontrados = list(filter(lambda x : x == p, self.__product_list))
+        
+        if len(encontrados) == 0:
+            self.__product_list.append(p)
 
     def list_products(self)-> str:
-        products_info = ""
-        for product in self.__product_list:
-            products_info += f"Name: {product.get_name}, Cost: {product.get_cost}, Stock: {product.get_stock}\n"
-        return products_info
+        if len(self.__product_list):
+            retorno = ''
+
+            for p in self.__product_list:
+                retorno += f'Name: {p.get_name},\n'\
+                            f'Cost: {p.get_cost}, \n'
+            return retorno
+
+        else:
+            return 'no hay productos'
     
     def sales(self, product_name:str, cantidad:int):
-        for product in self.__product_list:
-            if product.get_name == product_name:
-                current_stock = product.get_stock
-                if current_stock >= cantidad:
-                    product.set_stock(current_stock - cantidad)
-                    print(f"{cantidad} {product_name} sold successfully.")
-                else:
-                    print(f"Not enough stock for {cantidad} {product_name}.")
-                return
-        print(f"{product_name} not found in the product list.")
+        pass
 
 
 class Farmacia(Tienda):
-    pass
+    def __init__(self, name, delivery_cost):
+        self.__name = name
+        self.__delivery_cost = delivery_cost
+        self.__product_list = []
+    
+    def ingress_products(self, name:str, cost:int, stock:int = 0):
+        p = Product(name, cost, stock)
+        encontrados = list(filter(lambda x : x == p, self.__product_list))
+        
+        if len(encontrados) == 0:
+            self.__product_list.append(p)
+        else:
+            indice = self.__product_list.index(p)
+            self.__product_list[indice].get_stock = p + self.__product_list[indice]
+
+    def list_products(self)-> str:
+        if len(self.__product_list):
+            retorno = ''
+
+            for p in self.__product_list:
+                m = ''
+                if p.get_cost > 15000:
+                    m = 'envio gratis al solicitar el producto'
+                retorno += f'Name: {p.get_name}, \n'\
+                            f'Cost: {p.get_cost}, {m}\n'
+            return retorno
+
+        else:
+            return 'no hay productos'
+    
+    def sales(self, product_name:str, cantidad:int):
+        if cantidad < 3:
+            p = Product(product_name, 0, cantidad)
+            encontrados = list(filter(lambda x : x == p, self.__product_list))
+
+            if len(encontrados) and encontrados[0].get_stock:
+                tmp = encontrados[0] - p
+                nuevo_stock = tmp if tmp > 0 else 0
+                indice = self.__product_list.index(p)
+                self.__product_list[indice].get_stock = nuevo_stock
 
 class Supermercado(Tienda):
-    pass
+    def __init__(self, name, delivery_cost):
+        self.__name = name
+        self.__delivery_cost = delivery_cost
+        self.__product_list = []
+    
+    def ingress_products(self, name:str, cost:int, stock:int = 0):
+        p = Product(name, cost, stock)
+        encontrados = list(filter(lambda x : x.get_name == p.get_name, self.__product_list))
+        if len(encontrados) == 0:
+            self.__product_list.append(p)
+        else:
+            indice = self.__product_list.index(encontrados[0])
+            self.__product_list[indice].set_stock += stock
+
+    def list_products(self)-> str:
+        if len(self.__product_list):
+            retorno = ''
+
+            for p in self.__product_list:
+                m = ''
+                if p.get_stock < 10:
+                    m = 'pocos productos disponibles'
+                retorno += f'Name: {p.get_name},\n'\
+                            f'Cost: {p.get_cost},\n'\
+                            f'Stock: {p.get_stock} {m}\n'
+            return retorno
+
+        else:
+            return 'no hay productos'
+    
+    def sales(self, product_name:str, cantidad:int):
+
+            p = Product(product_name, 0, cantidad)
+            encontrados = list(filter(lambda x : x == p, self.__product_list))
+
+            if len(encontrados) and encontrados[0].get_stock:
+                tmp = encontrados[0] - p
+                nuevo_stock = tmp if tmp > 0 else 0
+                indice = self.__product_list.index(p)
+                self.__product_list[indice].get_stock = nuevo_stock
 
 if __name__ == "__main__":
-    tienda = Restaurante('tienda_name', 1000)
-    tienda.ingress_products('nombre_producto', 120, 3)
-    tienda.ingress_products('nombre_pr', 120, 1)
+    tienda = Supermercado('tienda_name', 1000)
+    tienda.ingress_products('nombre_producto', 120, 10)
+    tienda.ingress_products('nombre_pr', 120000, 1)
     tienda.ingress_products('nombre_producto', 120, 3)
     print(tienda.list_products())
